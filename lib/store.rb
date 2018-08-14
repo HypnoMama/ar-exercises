eda0c373363935bb46ec2f905211ca4fb1570139
HypnoMama <sarahhermsen@gmail.com>
2018-08-14 20:21:31 +0000

    added validators and a custom validator

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
+    has_many :employees
+
+    validates :name, presence: true, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+    validates :mens_apparel, :womens_apparel, inclusion: { in: [true, false] }
+    validate :must_sell_one_of
+
+    def must_sell_one_of
+        if mens_apparel == false && womens_apparel == false
+            errors.add(:mens_apparel, :womens_apparel, "mens_apparel and womens_apparel cannot both be false")
+        end
+    end
 
 end
