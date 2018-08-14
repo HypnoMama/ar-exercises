503d62b0b3cbec4f01fd426ca65547da109b831e
HypnoMama <sarahhermsen@gmail.com>
2018-08-14 18:52:17 +0000

    added 3 more stores and queried based n conditions, looped through results to display

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,22 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true
+Store.create name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false
+Store.create name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true
+
+@mens_stores = Store.where(womens_apparel: false)
+
+@mens_stores.each do |store|  
+    puts "store: #{store.name}, annual revenue: #{store.annual_revenue}."
+end
+
+@womens_stores = Store.where(womens_apparel: true)
+
+@womens_stores.each do |store|
+    if store.annual_revenue < 1000000
+        puts "store: #{store.name}, annual revenue: #{store.annual_revenue}."
+    end
+end
+
+puts Store.count
