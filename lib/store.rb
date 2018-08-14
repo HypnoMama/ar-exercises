4296a661ebb5dcc034e2796e45dd54b062b76243
HypnoMama <sarahhermsen@gmail.com>
2018-08-14 21:31:06 +0000

    added method to disallow store destruction

--- lib/store.rb
+++ lib/store.rb
@@ -6,10 +6,20 @@ class Store < ActiveRecord::Base
     validates :mens_apparel, :womens_apparel, inclusion: { in: [true, false] }
     validate :must_sell_one_of
 
+    before_destroy :check_if_employees
+
     def must_sell_one_of
         if mens_apparel == false && womens_apparel == false
             errors.add(:mens_apparel, :womens_apparel, "mens_apparel and womens_apparel cannot both be false")
         end
     end
+
+    private
+        def check_if_employees
+            if employees.size > 0
+                puts "Sorry you may not delete that store"
+                throw :abort
+            end
+        end
 
 end
